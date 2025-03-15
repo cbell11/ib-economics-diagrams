197c058f165d0df81129087b9df4349c03b00ab8
cbell11 <chandler0522@gmail.com>
2025-03-15 23:26:27 +0100

    Remove Firebase and auth temporarily

--- components/ClientRootLayout.tsx
+++ components/ClientRootLayout.tsx
@@ -1,12 +1,5 @@
 'use client';
 
-import dynamic from 'next/dynamic';
-
-const AuthProviderWrapper = dynamic(
-  () => import('./AuthProviderWrapper'),
-  { ssr: false }
-);
-
 export default function ClientRootLayout({ children }: { children: React.ReactNode }) {
-  return <AuthProviderWrapper>{children}</AuthProviderWrapper>;
+  return <>{children}</>;
 } 
